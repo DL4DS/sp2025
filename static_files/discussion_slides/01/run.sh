be6467efdfcb2226a655c6e4042190650564527b
xavierohan <144237146+XThomasBU@users.noreply.github.com>
2025-01-31 06:04:21 -0500

    update

--- static_files/discussion_slides/01/run.sh
+++ static_files/discussion_slides/01/run.sh
@@ -10,4 +10,4 @@ conda activate dl4ds # activate your conda environment
 python 01_model_intro.py
 
 # To submit the job to SCC, run the following command
-# qsub -pe omp 4 -P ds598 -l gpus=1 -o output.txt -e error.txt run.sh -- Copy paste this command in your terminal!
+# qsub -pe omp 4 -P dl4ds -l gpus=1 -o output.txt -e error.txt run.sh -- Copy paste this command in your terminal!
